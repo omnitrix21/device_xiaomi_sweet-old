1b334e516b5f7eb4fbb9415ee55e891865212d45
omnitrix21 <okand7946@gmail.com>
2023-10-20 11:31:44 +0530

    sweet: vendorsetup.sh: update kernel link and branches
    also dropped clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,14 @@
-# clone
-
-#clang
-git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git prebuilts/clang/host/linux-x86/r487747c
-
 #kernel
-git clone -b t13 --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b lineage-20 --depth=1 https://github.com/pulkit077/kernel_xiaomi_sweet.git kernel/xiaomi/sweet
 
 #common_vendor
-git clone https://github.com/omnitrix21/android_vendor_xiaomi_sm6150-common.git -b 13 vendor/xiaomi/sm6150-common
+git clone https://github.com/omnitrix21/android_vendor_xiaomi_sm6150-common.git -b U vendor/xiaomi/sm6150-common
 
 #common_tree
-git clone https://github.com/omnitrix21/device_xiaomi_sm6150-common.git -b thirteen device/xiaomi/sm6150-common
+git clone https://github.com/omnitrix21/device_xiaomi_sm6150-common.git -b U device/xiaomi/sm6150-common
 
 #sweet_vendor
-git clone https://github.com/omnitrix21/android_vendor_xiaomi_sweet.git -b 13 vendor/xiaomi/sweet
+git clone https://github.com/omnitrix21/android_vendor_xiaomi_sweet.git -b U vendor/xiaomi/sweet
 
 #remove
 rm -rf hardware/xiaomi
@@ -21,8 +16,3 @@ rm -rf hardware/xiaomi
 #hardware_xiaomi
 git clone https://github.com/PixysOS-Devices/hardware_xiaomi  -b thirteen hardware/xiaomi
 
-#memecam
-git clone -b 13-pixys https://gitlab.com/okand7946/vendor_xiaomi_sweet-miuicamera vendor/xiaomi/sweet-miuicamera
-
-#sony dolby
-git clone -b 13  https://github.com/omnitrix21/sony_vendor_dolby vendor/dolby
